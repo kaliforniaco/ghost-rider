067a5f328ed020220533ae63362b7682afe92c82
Sergio Souza <emailsouza@gmail.com>
2018-08-17 09:33:49 -0600

    start of the day - apis working

--- ghost_rider/cars/admin.py
+++ ghost_rider/cars/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import (Car, Comment)
+from .models import (Car, Comment, User)
 
 # Register your models here.
 
